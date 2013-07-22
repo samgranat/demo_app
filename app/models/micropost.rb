586ffb8b9183d18187dc08bbfa47591929c4769e
SamG <samson@manilla.com>
2013-07-22 15:50:32 -0700

    Finish demo app

---
+++ app/models/micropost.rb
@@ -0,0 +1,11 @@
+class Micropost < ActiveRecord::Base
+end
+
+class Micropost < ActiveRecord::Base
+  validates :content, length: { maximum: 140 }
+end
+
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+  validates :content, length: { maximum: 140 }
+end
